f2e1545adfce09edc72b25f6a03152139e4be1fd
Muchui William <williammuchui584@gmail.com>
2024-09-06 10:34:43 +0300

    random commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import AgreementPage from "./pages/AgreementPage/AgreementPage";
-import LayoutPage from "./pages/LayoutPage/LayoutPage";
-import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
-import NoPage from "./pages/NoPage/NoPage";
+import HomePage from "./pages/HomePage/HomePage.tsx";
+import AgreementPage from "./pages/AgreementPage/AgreementPage.tsx";
+import LayoutPage from "./pages/LayoutPage/LayoutPage.tsx";
+import CheckoutPage from "./pages/CheckoutPage/CheckoutPage.tsx";
+import NoPage from "./pages/NoPage/NoPage.tsx";
 
 function App() {
   return (
